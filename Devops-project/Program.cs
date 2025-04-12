072255459f62a74e2bcd1e2bb77115586ee62f11
HHameem <hhameem@my.centennialcollege.ca>
2025-04-12 18:48:37 -0400

    Hello world app

---
+++ Devops-project/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Devops_project
+{
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Hello, World!");
+            }
+        }
+    }
+
